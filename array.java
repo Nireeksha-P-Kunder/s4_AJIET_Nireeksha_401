08390fabce87d00b7cd0b843a24fd51f097afe5e
Nireeksha-P-Kunder <nireekshapkunder@gmail.com>
2026-05-08 14:52:34 +0530

    Create array.java

---
+++ array.java
@@ -0,0 +1,11 @@
+class Main {
+    public static void main(String[] args) {
+        int [] a={3,7,5,2,9};
+        int l=a[0];
+        for (int i=0;i<a.length;i++){
+            if(a[i]>l)
+            l=a[i];
+        }
+        System.out.println("Largest element is:"+l);
+    }
+}
